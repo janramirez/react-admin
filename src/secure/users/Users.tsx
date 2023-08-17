4b6873fbd6b622239e412f3fb2a3d36cbf7de7da
janramirez <jramirez.ust@gmail.com>
2023-08-18 00:37:54 +0800

    Added pagination to users dashboard

---
+++ src/secure/users/Users.tsx
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import Wrapper from "../Wrapper";
+import axios from "axios";
+import { User } from "../../classes/user";
+import { Link } from "react-router-dom";
+
+class Users extends Component {
+  state = {
+    users: [],
+  }
+
+  page = 1;
+  last_page = 0;
+
+  componentDidMount = async () => {
+    const response = await axios.get(`users?page=${this.page}`);
+
+    this.setState({
+      users: response.data.data,
+    });
+
+    this.last_page = response.data.meta.last_page;
+  }
+
+  prev = async () => {
+    if(this.page === 1) return;
+
+    this.page--;
+
+    await this.componentDidMount();
+  }
+
+  next = async () => {
+    if(this.page === this.last_page) return;
+
+    this.page++;
+
+    await this.componentDidMount();
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <div className="btn-toolbar mb-2 mb-md-0">
+            <Link to={'/users/create'} className="btn btn-sm btn-outline-secondary">Add</Link>
+          </div>
+        </div>
+        <div className="table-responsive">
+          <table className="table table-striped table-sm">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.users.map((user: User) => {
+                return (
+                  <tr>
+                    <td>{user.id}</td>
+                    <td>
+                      {user.first_name} {user.last_name}
+                    </td>
+                    <td>{user.email}</td>
+                    <td>{user.role.name}</td>
+                    <td>
+                      <div className="btn-group mr-2">
+                        <a href="#" className="btn btn-sm btn-outline-secondary">Edit</a>
+                        <a href="#" className="btn btn-sm btn-outline-secondary">Delete</a>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+
+        <nav>
+          <ul className="pagination">
+            <li className="page-item">
+              <a href="#" className="page-link" onClick={this.prev}>Previous</a>
+            </li>
+            <li className="page-item">
+              <a href="#" className="page-link" onClick={this.next}>Next</a>
+            </li>
+          </ul>
+        </nav>
+      </Wrapper>
+    );
+  }
+}
+
+export default Users;

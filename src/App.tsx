121a6ea1c734e80f0eeeaa650749e1667e8e7cd2
janramirez <jramirez.ust@gmail.com>
2023-08-17 23:44:27 +0800

    Added user action elements

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import Users from "./secure/users/Users";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./public/Login";
 import Register from "./public/Register";
+import RedirectToDashboard from "./secure/RedirectToDashboard";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path={"/"} element={<Dashboard />} />
+          <Route path={"/"} element={<RedirectToDashboard />} />
+          <Route path={"/dashboard"} element={<Dashboard />} />
           <Route path={"/users"} element={<Users />} />
           <Route path={"/login"} element={<Login />} />
           <Route path={"/register"} element={<Register />} />
